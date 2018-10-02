41305e32d682eea70320ced1bf1815e5fcd434e6
botCatz314 <33142028+botCatz314@users.noreply.github.com>
2018-10-01 20:30:36 -0700

    commented code
    commented

--- TeleOpTest.java
+++ TeleOpTest.java
@@ -8,19 +8,19 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 @TeleOp(name = "TeleOp Test", group = "Default")
 public class TeleOpTest extends LinearOpMode {
 
-    private DcMotor left, right;
+    private DcMotor left, right; //declares motors
 @Override
     public void runOpMode()
 {
 
-    left = hardwareMap.dcMotor.get("left");
-    right = hardwareMap.dcMotor.get("right");
+    left = hardwareMap.dcMotor.get("left"); //sets value to left motor
+    right = hardwareMap.dcMotor.get("right"); //sets value to right motor
 
     waitForStart();
     while (opModeIsActive())
     {
-        left.setPower(-gamepad1.left_stick_y);
-        right.setPower(-gamepad1.right_stick_y);
+        left.setPower(-gamepad1.left_stick_y); //sets left motor power
+        right.setPower(-gamepad1.right_stick_y); //sets right motor power
 
         idle();
     }
