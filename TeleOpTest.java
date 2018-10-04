c04e6abd5c830ce7d2f2d30d60ee61901132ad1a
botCatz314 <33142028+botCatz314@users.noreply.github.com>
2018-10-03 19:04:14 -0700

    remove comment
    removed comment "example"

--- TeleOpTest.java
+++ TeleOpTest.java
@@ -12,7 +12,6 @@ public class TeleOpTest extends LinearOpMode {
 @Override
     public void runOpMode()
 {
-    //example
     left = hardwareMap.dcMotor.get("left"); //sets value to left motor
     right = hardwareMap.dcMotor.get("right"); //sets value to right motor
 
