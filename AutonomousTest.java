182fca82850a5254469bd0980e2ff220434136d7
botCatz <botCatz314@gmail.com>
2018-10-06 13:08:36 -0700

    comments
    commented set up for autonomous

---
+++ AutonomousTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous (name = "AutonomousTest")
+public class AutonomousTest extends LinearOpMode {
+
+    private DcMotor left, right; // declare drive motor variables
+@Override
+    public void runOpMode()
+{
+    left = hardwareMap.dcMotor.get("left"); //set left drive motor
+    right = hardwareMap.dcMotor.get("right"); //set right drive motor
+
+    left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //sets left motor to run without encoder
+    right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //sets right motor to run without encoder
+
+    waitForStart();
+}
+
+
+
+
+
+
+
+}
