2e2ded343204b4eba5659765b6102c278864c8eb
botCatz <botCatz314@gmail.com>
2018-10-06 13:54:18 -0700

    stop bot method
    Created a stop bot method and implemented it so the bot will stop after five seconds.

--- AutonomousTest.java
+++ AutonomousTest.java
@@ -8,25 +8,25 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class AutonomousTest extends LinearOpMode {
 
     private DcMotor left, right; // declare drive motor variables
+    public double stopBot = 0;
 @Override
     public void runOpMode() {
     left = hardwareMap.dcMotor.get("left"); //set left drive motor
     right = hardwareMap.dcMotor.get("right"); //set right drive motor
-
     left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //sets left motor to run without encoder
     right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //sets right motor to run without encoder
 
     waitForStart();
     MoveRobot(1, 1);
+    sleep(5000);
+    StopBot();
 }
     private void MoveRobot(double leftPower, double rightPower){
     right.setPower(rightPower);
     left.setPower(leftPower);
     }
-
-
-
-
-
-
+    private void StopBot(){
+    left.setPower(stopBot);
+    right.setPower(stopBot);
+    }
 }
