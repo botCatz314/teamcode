14772f7d2aee153f169a4d8dcadb8211591cd475
botCatz314 <33142028+botCatz314@users.noreply.github.com>
2018-10-08 20:37:49 -0700

    Update Jailyn.java
    teach jalin to code

---
+++ Jailyn.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous (name = "AutonomousTest")
+public class Jailyn extends LinearOpMode {
+
+    private DcMotor left, right; // declare drive motor variables
+    public double stopBot = 0;
+@Override
+    public void runOpMode() {
+    left = hardwareMap.dcMotor.get("left"); //set left drive motor
+    right = hardwareMap.dcMotor.get("right"); //set right drive motor
+    left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //sets left motor to run without encoder
+    right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //sets right motor to run without encoder
+
+    waitForStart();
+    MoveRobot(1, 1);
+    sleep(5000);
+    StopBot();
+}
+
+
+} private void MoveRobot(double leftPower, double rightPower){
+        right.setPower(rightPower);
+        left.setPower(leftPower);
+    }
+    private void StopBot(){
+        left.setPower(stopBot);
+        right.setPower(stopBot);
