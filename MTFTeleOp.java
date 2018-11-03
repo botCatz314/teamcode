778d90cc287bbe6c7ffa09bfb2aecdcc241c4393
botCatz314 <33142028+botCatz314@users.noreply.github.com>
2018-11-02 19:59:53 -0700

    Update MTFTeleOp.java

---
+++ MTFTeleOp.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+@TeleOp(name = "MTF TeleOp", group = "Default")
+public class MTFTeleOp extends LinearOpMode {
+
+    private DcMotor left, left1, right1, right, hangingMotor, pivotMotor, slideMotor; //declares
+    private DigitalChannel touchLower, touchUpper;
+
+    private boolean collecting;
+@Override
+    public void runOpMode()
+{
+    left = hardwareMap.dcMotor.get("left"); //sets value to left motor
+    right = hardwareMap.dcMotor.get("right"); //sets value to right motor
+    left1 = hardwareMap.dcMotor.get(("left1"));
+    right1 = hardwareMap.dcMotor.get("right1");
+    hangingMotor = hardwareMap.dcMotor.get("hangingMotor");
+    touchLower = hardwareMap.get(DigitalChannel.class, "touchLower");
+    touchUpper = hardwareMap.get(DigitalChannel.class, "touchUpper");
+    pivotMotor = hardwareMap.get("pivotMotor");
+    slideMotor = hardwareMap.get("slideMotor");
+
+
+
+
+
+    touchLower.setMode(DigitalChannel.Mode.INPUT);
+    touchUpper.setMode(DigitalChannel.Mode.INPUT);
+
+
+    left.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    waitForStart();
+    while (opModeIsActive())
+    {
+        left.setPower(gamepad1.left_stick_y); //sets left motor power
+        left1.setPower(gamepad1.left_stick_y);
+        right.setPower(-gamepad1.right_stick_y); //sets right motor power
+        right1.setPower(-gamepad1.right_stick_y);
+
+        gamepad2.x {
+            if (collecting){
+                collecting = false
+            }
+            else if (!collecting){
+                collecting = true
+            }
+    }
+
+    slideMotor.setPower(gamepad2.right_stick_y);
+        pivotMotor.setPower(gamepad2.left_stick_y);
+
+        idle();
+    }
+}
+
+
+
+
+
+
+
+
+}
