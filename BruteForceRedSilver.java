f6e1dca9e117edf4800cbe423914e41e6b976d4b
botCatz314 <33142028+botCatz314@users.noreply.github.com>
2018-11-20 20:25:09 -0800

    Luke's weird stuff
    Scores the left jewel

---
+++ BruteForceRedSilver.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.teamcode;
+
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.DogeCV;
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous (name = "BruteForceRedSilver")
+public class BruteForceRedSilver extends LinearOpMode {
+
+    private DcMotor left, right; // declare drive motor variables
+
+    private double powerOff = 0; //declares common powers that we use
+@Override
+    public void runOpMode() {
+    //sets drive motors
+    left = hardwareMap.dcMotor.get("left");
+    right = hardwareMap.dcMotor.get("right");
+   //sets up drive motors to our specification
+    left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    left.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    waitForStart();
+    left.setPower(.5);
+    right.setPower(1);
+    sleep(1500);
+
+}
+
+}
+
+
--- MTFTeleOp.java
+++ MTFTeleOp.java
@@ -11,8 +11,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "MTF TeleOp", group = "Default")
 public class MTFTeleOp extends LinearOpMode {
 
-    private DcMotor left, left1, right1, right, hangingMotor, pivotMotor, slideMotor; //declares
-    private DigitalChannel touchLower, touchUpper;
+    private DcMotor left, right,  pivotMotor, slideMotor; //declares
     private CRServo collector;
 
     private boolean collecting;
@@ -21,11 +20,6 @@ public class MTFTeleOp extends LinearOpMode {
 {
     left = hardwareMap.dcMotor.get("left"); //sets value to left motor
     right = hardwareMap.dcMotor.get("right"); //sets value to right motor
-    left1 = hardwareMap.dcMotor.get(("left1"));
-    right1 = hardwareMap.dcMotor.get("right1");
-    hangingMotor = hardwareMap.dcMotor.get("hangingMotor");
-    touchLower = hardwareMap.get(DigitalChannel.class, "touchLeft");
-    touchUpper = hardwareMap.get(DigitalChannel.class, "touchRight");
     pivotMotor = hardwareMap.dcMotor.get("pivotMotor");
     slideMotor = hardwareMap.dcMotor.get("slideMotor");
     collector = hardwareMap.crservo.get("collector");
@@ -34,21 +28,18 @@ public class MTFTeleOp extends LinearOpMode {
 
 
 
-    touchLower.setMode(DigitalChannel.Mode.INPUT);
-    touchUpper.setMode(DigitalChannel.Mode.INPUT);
+
 
 
     left.setDirection(DcMotorSimple.Direction.REVERSE);
-    left1.setDirection(DcMotorSimple.Direction.REVERSE);
-    hangingMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
 
     waitForStart();
     while (opModeIsActive())
     {
         left.setPower(gamepad1.left_stick_y); //sets left motor power
-        left1.setPower(gamepad1.left_stick_y);
+
         right.setPower(gamepad1.right_stick_y); //sets right motor power
-        right1.setPower(gamepad1.right_stick_y);
 
         if(gamepad2.x)
         {
@@ -65,8 +56,7 @@ public class MTFTeleOp extends LinearOpMode {
         slideMotor.setPower(gamepad2.right_stick_y);
         pivotMotor.setPower(gamepad2.left_stick_y);
 
-        hangingMotor.setPower(gamepad1.left_trigger);
-        hangingMotor.setPower(-gamepad1.right_trigger);
+
 
         idle();
     }
