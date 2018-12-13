5c9203371f7c9ccb3cab8602c5a7c0c48384a3ba
botCatz314 <33142028+botCatz314@users.noreply.github.com>
2018-12-12 20:55:48 -0700

    updated teleOp

--- botCatzTeleOp.java
+++ botCatzTeleOp.java
@@ -92,6 +92,14 @@ public class botCatzTeleOp extends LinearOpMode {
             double velL = 0;
             double velR = 0;
             motorIsUsed = true;
+        if(gamepad2.dpad_up){
+            velL = 0.2;
+            velR = 0.2;
+        }
+        if(gamepad2.dpad_down){
+            velL = -0.2;
+            velR = -0.2;
+        }
         if (gamepad1.left_stick_y >= 0.1 || gamepad1.left_stick_y <= -0.1) {
             velL = gamepad1.left_stick_y;
         }
@@ -124,7 +132,7 @@ public class botCatzTeleOp extends LinearOpMode {
             armToScoringPosition(-0.5);
         }
 
-        if(gamepad2.dpad_up && !getTouch(touchUpper)){
+        if(gamepad2.y && !getTouch(touchUpper)){
             driveUntilTouch(1.0, hangingMotor, touchUpper);
         }
         if(gamepad2.right_trigger > 0.1) {
