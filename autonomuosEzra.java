b0c957e7fa2b1441342758bd4834c49115cf03b6
botCatz314 <33142028+botCatz314@users.noreply.github.com>
2019-01-30 20:51:06 -0700

    disabled rest of junk

--- autonomuosEzra.java
+++ autonomuosEzra.java
@@ -1,9 +1,10 @@
 package org.firstinspires.ftc.teamcode.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+@Disabled
 @Autonomous (name = "AutonomousEzra")
 public class autonomuosEzra extends LinearOpMode {
 
